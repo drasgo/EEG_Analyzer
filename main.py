Bug fixes for Json saving and acquisition + wavelet computation

main.py
@@ -222,6 +222,7 @@ def show_progress_train(var=None):
 
 def teach_nn(lsl, name):
     show_progress_train("Starting Signal acquisition ...")
+    path_name = "virtenv/resources/" + name + "_data.json"
     count = 0
     try:
         while stop_teaching is False:
@@ -246,52 +247,36 @@ def teach_nn(lsl, name):
             show_progress_train("Done!")
             finaldata = data[0:lsl.sample_rate*__signalAcquisitionTime__]
             show_progress_train("Saving current sample ...")
-            nome_file = save_samples(finaldata, name, arrow)
+            save_samples(finaldata, name, arrow)
     except:
         err_page("Error acquiring EEG data!")
 
     show_progress_train("Finishing Signal acquisition ...")
+    show_progress_train("Data saved in " + path_name)
     stop_acq_data_teach_nn(lsl)
-    train_nn(nome_file, lsl.sample_rate)
+    train_nn(path_name, lsl.sample_rate, name)
 
 
-def save_samples(data, name, arrow):
-    path_name = "virtenv/resources/"+name+"_data.json"
-
+def save_samples(data, path_name, arrow):
     if os.path.exists(os.path.abspath(path_name)):
         temp_dict = json.load(open(os.path.abspath(path_name)))
-        temp = {}
-        o = 0
-        for x in temp_dict:  ##acquisizione
-            count_temp = []
-            for k in x:  ## arrow + dati
-                count_temp.append(k)
-            temp[str(o)] = count_temp
-            o += 1
-        count = len(temp) + 1
+        count = len(temp_dict)
     else:
         temp_dict = {}
         count = 0
 
     temp_ch = []
-    temp_glob = []
-
-    temp_glob.append(arrow)
-
+    temp_glob = {}
+    temp_glob["arrow"] = arrow
     for chan in data:
         temp_ch.append(chan)
 
-    temp_glob.append(temp_ch)
+    temp_glob["dati"] = temp_ch
 
-    ttemp = []
-    ttemp.append(temp_glob)
-
-    temp_dict[str(count)] = ttemp
+    temp_dict[str(count)] = temp_glob
 
     with open(os.path.abspath(path_name), 'w') as op:
         json.dump(temp_dict, op, indent=4)
-
-    return path_name
 
 
 def get_samples(name):
@@ -301,8 +286,18 @@ def get_samples(name):
 def wavelet_analysis(data, sample_rate):
     coef = []
     freq = []
+    arrow = []
+    dati = []
+    canali = []
 
-    for sampl in data:
+    for sing_acqu in np.arange(0, len(data)):
+        acq = data[str(sing_acqu)]
+        arrow.append(acq["arrow"])
+        for sing_chn in acq["dati"]:
+            canali.append(sing_chn)
+        dati.append(canali)
+
+    for sampl in dati:
         part_coef=[]
         part_freq=[]
 
@@ -312,9 +307,9 @@ def wavelet_analysis(data, sample_rate):
             threshold = signal_to_noise_ratio(temp_coef)
 
             for j in temp_coef:
-                for k in temp_coef[j]:
-                    if temp_coef[j][k] < threshold:
-                        temp_coef[j][k] = 0
+                for k in j:
+                    if k < threshold:
+                        k = 0
 
             part_coef.append(temp_coef)
             part_freq.append(temp_freq)
@@ -322,7 +317,7 @@ def wavelet_analysis(data, sample_rate):
         coef.append(part_coef)
         freq.append(part_freq)
 
-    return coef, freq
+    return coef, freq, arrow
 
 
 def signal_to_noise_ratio(data):
@@ -330,19 +325,19 @@ def signal_to_noise_ratio(data):
     sum = 0
     count = 0
     for i in data:
-        for j in data[i]:
-            if max_val < data[i][j]:
-                max_val = data[i][j]
-            sum += data[i][j]
+        for j in i:
+            if max_val < j:
+                max_val = j
+            sum += j
             count += 1
     return max_val/(sum/count)
 
 
-def train_nn(path, arrow, sample_rate):
+def train_nn(path, sample_rate, user):
     show_progress_train("Passing through Wavelet Analisys ...")
-    coef, freq = wavelet_analysis(get_data(path), sample_rate)
+    coef, freq, arrow = wavelet_analysis(get_data(path), sample_rate)
     show_progress_train("Done!")
-    nn = neu_net.NeuralNetwork(coef, freq, arrow)
+    nn = neu_net.NeuralNetwork(coef, freq, arrow, user)
 
     return nn
 

temp/wavelet_trial.py
@@ -10,14 +10,63 @@ import json
 import configparser
 import matplotlib.axes as ax
 import time
+import random
 
-class Vividict(dict):
-    def __getitem__(self, item):
-        try:
-            return dict.__getitem__(self, item)
-        except KeyError:
-            value = self[item] = type(self)()
-            return value
+
+
+def wavelet_analysis(data, sample_rate):
+    coef = []
+    freq = []
+    arrow = []
+    dati = []
+    canali = []
+
+    for sing_acqu in np.arange(0, len(data)):
+        acq = data[str(sing_acqu)]
+        arrow.append(acq["arrow"])
+        for sing_chn in acq["dati"]:
+            canali.append(sing_chn)
+        dati.append(canali)
+
+    for sampl in dati:
+        part_coef=[]
+        part_freq=[]
+
+        for chan in sampl:
+            scale = np.arange(1, 15)
+            temp_coef, temp_freq = pywt.cwt(chan, scale, "morl", sample_rate)
+            threshold = signal_to_noise_ratio(temp_coef)
+
+            for j in temp_coef:
+                for k in j:
+                    if k < threshold:
+                        print(k+ "--> 0")
+                        k = 0
+
+            part_coef.append(temp_coef)
+            part_freq.append(temp_freq)
+
+        coef.append(part_coef)
+        freq.append(part_freq)
+
+    return coef, freq, arrow
+
+
+def signal_to_noise_ratio(data):
+    max_val = 0
+    sum = 0
+    count = 0
+    for i in data:
+        for j in i:
+            if max_val < j:
+                max_val = j
+            sum += j
+            count += 1
+#    print(str(max_val)+"/("+str(sum)+"/"+str(count)+")")
+    return max_val/(sum/count)
+
+
+
 #with open(os.path.abspath("virtenv/bin/mauna.dat")) as o:
 #    data = np.loadtxt(os.path.abspath("virtenv/bin/sst_nino3.dat"))
 #print (data)
@@ -32,37 +81,36 @@ sampl_per=0.25
 #coef, freq = pywt.cwt(y, np.arange(1,15), 'morl', sampl_per)
 
 tremp = np.array(y).tolist()
-a = "a"
+
+name = "Tom"
+path_name = name + "_data.json"
 
 for i in np.arange(0, 10):
-    name="Tom"
-    path_name = name+"_data.json"
     if os.path.exists(os.path.abspath(path_name)):
         temp_dict = json.load(open(os.path.abspath(path_name)))
-        temp = {}
-        o = 0
-        for x in temp_dict: ##acquisizione
-            count_temp = []
-            for k in x:## arrow + dati
-                count_temp.append(k)
-            temp[str(o)]=count_temp
-            o+=1
-        count = len(temp) + 1
+    #    temp = {}
+   #     o = 0
+ #       for x in np.arange(0, len(temp_dict)): ##acquisizione
+  #           count_temp = []
+  #          for k in temp_dict[str(x)]:## arrow + dati
+  #              count_temp.append(k)
+  #          temp[str(o)]=count_temp
+  #          o+=1
+        count = len(temp_dict)
 
     else:
         temp_dict = {}
         count = 0
 
+    a = random.choice(["right", "left"])
     temp_ch=[]
-    temp_glob = []
-    temp_glob.append(a)
+    temp_glob = {}
+    temp_glob["arrow"] = a
     for k in np.arange(0,10):
         temp_ch.append(tremp)
-    temp_glob.append(temp_ch)
-    ttemp = []
-    ttemp.append(temp_glob)
+    temp_glob["dati"] = temp_ch
 
-    temp_dict[str(count)]=ttemp
+    temp_dict[str(count)]=temp_glob
 
     with open(os.path.abspath(path_name), 'w') as op:
         json.dump(temp_dict, op, indent=4)
@@ -73,8 +121,38 @@ with open(os.path.abspath(path_name)) as op:
     dat=json.load(op)
 
 
-for k in dat:
-    coef, freq= pywt.cwt(y, np.arange(1,15), "morl", sampl_per)
+
+#pprint.pprint(dat)
+print("finito json dump")
+#pprint.pprint(dat["0"])
+#print(len(dat))
+#freccia = []
+#dati = []
+#canali = []
+
+#for k in np.arange(0, len(dat)):
+#    print(k)
+#    ins = dat[str(k)]
+#    freccia.append(ins["freccia"])
+#    print(ins["freccia"])
+#    for jj in ins["dati"]:
+#        canali.append(jj)
+#    dati.append(canali)
+
+#pprint.pprint(len(freccia))
+#pprint.pprint(len(dati))
+
+coef, freq, flag = wavelet_analysis(dat, sampl_per)
+
+print(len(coef))
+print(len(freq))
+print(len(flag))
+
+
+#    for kss in k:
+#        pprint.pprint(kss)
+
+#    coef, freq= pywt.cwt(y, np.arange(1,15), "morl", sampl_per)
 #    pprint.pprint(coef)
 #    pprint.pprint(freq)
 

virtenv/include/neural_network.py
@@ -1,4 +1,4 @@
-import tensorflow
+import tensorflow as tf
 
 
 class NeuralNetwork(object):
@@ -7,8 +7,8 @@ class NeuralNetwork(object):
         self.coef = coef
         self.freq = freq
         self.arrow = arrow
-        self.user = user
-        self.setup_nn()
+        self.path_name = "nn_" + user
+        setup_nn()
 
     def setup_nn(self):
         if
@@ -21,6 +21,8 @@ class NeuralNetwork(object):
         print()
 
     def save_nn(self):
+        saver = tf.train.Saver()
+        saver.save(sess, self.path_name)
         print()
 
     def run_nn(self):